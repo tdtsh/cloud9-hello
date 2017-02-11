e540236b0400e8fe0a413ff940be685f75b0f2e3
tdtsh <systemsfailing@gmail.com>
2017-02-11 08:24:33 +0000

    modify root

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'application#hello'
+  # root 'application#hello'
+  root 'articles#index'
 end
